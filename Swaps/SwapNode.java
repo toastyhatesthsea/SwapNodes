956529c668f5013956a8ab91070a09dc12c60344
Saena <toastyhatesthesea@gmail.com>
2018-09-24 22:42:45 -0700

    Rearranged classes and added code to swapPairs

--- Swaps/SwapNode.java
+++ Swaps/SwapNode.java
@@ -3,37 +3,64 @@ package Swaps;
 public class SwapNode
 {
 
+    public ListNode swapPairs(ListNode head)
+    {
+        ListNode current = head;
+        ListNode previous = current;
 
-     public class ListNode {
-        int val;
-         ListNode next;
 
-         ListNode(int x)
-         {
-             val = x;
-         }
-     }
-
-    class Swap {
-        public ListNode swapPairs(ListNode head)
+        while (current != null)
         {
-            ListNode current = head;
-            ListNode previous = current;
+            int firstValue = current.val;
+            int secondValue;
 
-
-            while (current != null)
+            if (current.next != null)
             {
-                int firstValue = current.val;
-                int secondValue;
+                ListNode nextCurrent = current.next;
+                current.next = current.next.next;
+                nextCurrent.next = current;
 
-                if (current.next != null)
-                {
-                    secondValue = current.next.val;
-                }
             }
-
+            else
+            {
+                break;
+            }
+            current = current.next.next;
         }
+        return current;
     }
 
+}
 
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x)
+    {
+        val = x;
+    }
+}
+
+class SwapTesters
+{
+    public static void main(String[] argsgs)
+    {
+
+
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(3);
+        l1.next.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(2);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+        l2.next.next.next = new ListNode(5);
+
+        SwapNode blahs = new SwapNode();
+
+
+
+    }
 }
