b3ba3f19aaf94060bf84ed63f662bba7d21e1f56
Saena <toastyhatesthesea@gmail.com>
2018-09-25 07:42:39 -0700

    Finished swapPairs function, added comments

--- Swaps/SwapNode.java
+++ Swaps/SwapNode.java
@@ -3,60 +3,56 @@ package Swaps;
 public class SwapNode
 {
 
+    /**Given a linked list, swap every two adjacent nodes and return its head.
+     Example:
+     Given 1->2->3->4, you should return the list as 2->1->4->3.
+     *
+     * @param head ListNode
+     * @return ListNode
+     */
     public ListNode swapPairs(ListNode head)
     {
         ListNode answer = head;
         ListNode current = head;
         ListNode previous = current;
         ListNode beforePrevious = current;
-        ListNode currentAnswerPointer = answer;
-
         boolean first = true;
-
 
         while (current != null)
         {
-
             if (previous != current)
             {
                 previous.next = current.next;
                 current.next = previous;
-                currentAnswerPointer = currentAnswerPointer.next;
 
                 if (first) // Get head of new linked list
                 {
                     answer = current;
-                    currentAnswerPointer = current;
                     first = false;
                 }
                 else
-                {
+                {   //Connects the node before previous to the node that was swapped
                     beforePrevious.next = current;
-                    //currentAnswerPointer.next = current;
-                    //currentAnswerPointer = currentAnswerPointer.next.next;
                 }
-
+                //checks to make sure loop is not at the end of the node and there are more nodes to swap
                 if (previous.next != null)
                 {
                     beforePrevious = previous;
-                    current = current.next.next.next;
+                    current = current.next.next.next; //moves current pointer for the current node to be swapped with 'previous' pointer
                     previous = previous.next;
                 }
                 else
                 {
                     break;
                 }
-
             }
             else
             {
-                previous = current;
                 current = current.next;
             }
         }
         return answer;
     }
-
 }
 
 class ListNode {
@@ -73,21 +69,26 @@ class SwapTesters
 {
     public static void main(String[] argsgs)
     {
+        SwapNode blahs = new SwapNode();
 
 
         ListNode l1 = new ListNode(1);
         l1.next = new ListNode(2);
         l1.next.next = new ListNode(3);
         l1.next.next.next = new ListNode(4);
+        l1.next.next.next.next = new ListNode(5);
+
+        ListNode l1List = blahs.swapPairs(l1);
 
         ListNode l2 = new ListNode(2);
         l2.next = new ListNode(3);
         l2.next.next = new ListNode(4);
         l2.next.next.next = new ListNode(5);
+        l2.next.next.next.next = new ListNode(7);
+        l2.next.next.next.next.next = new ListNode(9);
 
         l1.next.next.next.next = l2;
 
-        SwapNode blahs = new SwapNode();
         ListNode head = blahs.swapPairs(l1);
 
 
